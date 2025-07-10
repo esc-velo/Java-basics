116ba401a2399849cdc4ece61feddf370af4baa0
Rahini Raneru <rahiniraneru@yahoo.com>
2025-07-10 14:57:25 +0530

    Update interfaces.java

--- OOPS/interfaces.java
+++ OOPS/interfaces.java
@@ -1,12 +1,10 @@
-package masteryws;
-
-public class interfaces {
+public class Interfaces {
 
     public static void main(String[] args)
     {
         Flyable[] flyers = new Flyable[2];
-        flyers[0] = new bird();
-        flyers[1] = new plane();
+        flyers[0] = new Bird();
+        flyers[1] = new Plane();
 
         for (Flyable f : flyers){
             f.fly();
@@ -19,14 +17,14 @@ interface Flyable{
 }
 
 // keyword for interface extension in another class : implements
-class bird implements Flyable{
+class Bird implements Flyable{
     @Override
     public void fly() {
         System.out.println("Bird flies by flapping its wings.");
     }
 }
 
-class plane implements Flyable{
+class Plane implements Flyable{
     @Override
     public void fly() {
         System.out.println("Aeroplane flies using engines and wings.");
